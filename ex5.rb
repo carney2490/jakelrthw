e3e654d3be16651dc6165035a87768b3ec1f02a5
jacob <2staffi@gmail.com>
2016-08-18 23:23:42 -0400

    not luck fixing long decimal

--- ex5.rb
+++ ex5.rb
@@ -1,16 +1,16 @@
 name = 'Jacob C. Carroll'
-age = 33 # in 2016
-height = 71 # inches
-weight = 360 # lbs
+age = 33
+height = 71 * 2.54
+weight = 360 * 0.45
 eyes = 'Brown'
 teeth = 'White'
 hair = 'Brown'
 
 puts "Let's talk about #{name}."
-puts "He's #{height * 2.54} centimeters tall."
-puts "He's #{weight * 0.4} kilograms heavy."
+puts "He's #{height} centimeters tall."
+puts "He's #{weight} kilograms heavy."
 puts "Actually that's not heavy, it's just fat."
 puts "He's got #{eyes} eyes and #{hair} hair."
 puts "His teeth are usually #{teeth} depending on the coffee."
 
-puts "If I add #{age}, #{height * 2.54}, #{weight1 = weight * 0.4} I get #{height * 2.54 + weight1 + age}."
+puts "If I add #{age}, #{height}, #{weight} I get #{height + weight + age}."
