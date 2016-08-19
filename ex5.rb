9b320e618d9d747d8f90c0e6101fb9281bae211f
jacob <2staffi@gmail.com>
2016-08-18 22:56:22 -0400

    removed 'my_'

--- ex5.rb
+++ ex5.rb
@@ -1,16 +1,16 @@
-my_name = 'Jacob C. Carroll'
-my_age = 33 # in 2016
-my_height = 71 # inches
-my_weight = 360 # lbs
-my_eyes = 'Brown'
-my_teeth = 'White'
-my_hair = 'Brown'
+name = 'Jacob C. Carroll'
+age = 33 # in 2016
+height = 71 # inches
+weight = 360 # lbs
+eyes = 'Brown'
+teeth = 'White'
+hair = 'Brown'
 
-puts "Let's talk about #{my_name}."
-puts "He's #{my_height} inches tall."
-puts "He's #{my_weight} pounds heavy."
+puts "Let's talk about #{name}."
+puts "He's #{height} inches tall."
+puts "He's #{weight} pounds heavy."
 puts "Actually that's not heavy, it's just fat."
-puts "He's got #{my_eyes} eyes and #{my_hair} hair."
-puts "His teeth are usually #{my_teeth} depending on the coffee."
+puts "He's got #{eyes} eyes and #{hair} hair."
+puts "His teeth are usually #{teeth} depending on the coffee."
 
-puts "If I add #{my_age}, #{my_height}, #{my_weight} I get #{my_age + my_height + my_weight}."
+puts "If I add #{age}, #{height}, #{weight} I get #{age + height + weight}."
