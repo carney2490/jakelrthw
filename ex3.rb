010768827f154499d6c2b1149ae15d80c5348815
jacob <2staffi@gmail.com>
2016-08-18 21:03:31 -0400

    added comments to explain what the line does

--- ex3.rb
+++ ex3.rb
@@ -1,23 +1,33 @@
+# prints text about counting chickens
 puts "I will now count my chickens:"
 
+# prints text and an equation to give an answer
 puts "Hens #{25 + 30 / 6}"
 puts "Roosters #{100 - 25 * 3 % 4}"
 
+# prints text about counting eggs
 puts "Now I will count the eggs:"
 
+# an equation to count eggs
 puts 3 + 2 + 1 - 5 + 4 % 2 - 1 / 4 + 6
 
+# prints text asking a true false question
 puts "Is it true that 3 + 2 < 5 - 7?"
 
+# equation for true false question
 puts 3 + 2 < 5 - 7
 
+# prints text asking a question and then an equation to answer
 puts "What is 3 + 2? #{3 + 2}"
 puts "What is 5 - 7? #{5 - 7}"
 
+# prints text 
 puts "Oh, that's why it's false."
 
+# prints text
 puts "How about some more."
 
+# prints text and answers true false question
 puts "Is it greater? #{5 > -2}"
 puts "Is it greater or equal? #{5 >= -2}"
 puts "Is it less or equal? #{5 <= -2}"
