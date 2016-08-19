ba4fb9ac553f2cdca021e2e0c14de98731ef1b67
jacob <2staffi@gmail.com>
2016-08-18 23:56:41 -0400

    commented on lines

--- ex6.rb
+++ ex6.rb
@@ -1,21 +1,33 @@
+# setting types of people to 10
 types_of_people = 10
+# string using types of people variable
 x = "There are #{types_of_people} types of people."
+# setting binary to binary
 binary = "binary"
+# setting do_not to don't
 do_not = "don't"
+# setting y to string using binary and do_not variable
 y = "Those who know #{binary} and those who #{do_not}."
 
+# printing x and y string
 puts x
 puts y
 
+# pringing strings using x and y string
 puts "I said: #{x}."
 puts "I also said: '#{y}'."
 
+# setting halarious to false
 halarious = false
+# and joke_evaluation to string that has halarious variable in it
 joke_evaluation = "Isn't that joke so funny?! #{halarious}"
 
+# prints string
 puts joke_evaluation
 
+# sets w and e to two strings that make one sentence
 w = "This is the left side of..."
 e = "a string with a right side."
 
+# prints strings w and e
 puts w + e
