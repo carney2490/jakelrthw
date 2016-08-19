614258c3bad4db7e3d99b5b531634463aaa1d77d
jacob <2staffi@gmail.com>
2016-08-18 23:18:21 -0400

    converted lbs to kg and tried to stop long decimal

--- ex5.rb
+++ ex5.rb
@@ -8,9 +8,9 @@ hair = 'Brown'
 
 puts "Let's talk about #{name}."
 puts "He's #{height * 2.54} centimeters tall."
-puts "He's #{weight} pounds heavy."
+puts "He's #{weight * 0.4} kilograms heavy."
 puts "Actually that's not heavy, it's just fat."
 puts "He's got #{eyes} eyes and #{hair} hair."
 puts "His teeth are usually #{teeth} depending on the coffee."
 
-puts "If I add #{age}, #{height * 2.54}, #{weight} I get #{age + height * 2.54 + weight}."
+puts "If I add #{age}, #{height * 2.54}, #{weight1 = weight * 0.4} I get #{height * 2.54 + weight1 + age}."
