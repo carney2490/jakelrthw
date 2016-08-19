35abd032bd0c1f9b77f4892dc9ffda4780cc3f86
jacob <2staffi@gmail.com>
2016-08-18 23:03:30 -0400

    converted in to cm

--- ex5.rb
+++ ex5.rb
@@ -7,10 +7,10 @@ teeth = 'White'
 hair = 'Brown'
 
 puts "Let's talk about #{name}."
-puts "He's #{height} inches tall."
+puts "He's #{height * 2.54} centimeters tall."
 puts "He's #{weight} pounds heavy."
 puts "Actually that's not heavy, it's just fat."
 puts "He's got #{eyes} eyes and #{hair} hair."
 puts "His teeth are usually #{teeth} depending on the coffee."
 
-puts "If I add #{age}, #{height}, #{weight} I get #{age + height + weight}."
+puts "If I add #{age}, #{height * 2.54}, #{weight} I get #{age + height * 2.54 + weight}."
