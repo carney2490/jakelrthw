4283df08c3c50c737b76ff1d2a52d5c2bc5aa822
jacob <2staffi@gmail.com>
2016-08-18 21:11:18 -0400

    added floating '.' to change equations

--- ex3.rb
+++ ex3.rb
@@ -2,24 +2,24 @@
 puts "I will now count my chickens:"
 
 # prints text and an equation to give an answer
-puts "Hens #{25 + 30 / 6}"
-puts "Roosters #{100 - 25 * 3 % 4}"
+puts "Hens #{25.75 + 30 / 6.2}"
+puts "Roosters #{100 - 25.5 * 3 % 4}"
 
 # prints text about counting eggs
 puts "Now I will count the eggs:"
 
 # an equation to count eggs
-puts 3 + 2 + 1 - 5 + 4 % 2 - 1 / 4 + 6
+puts 3.2 + 2 + 1.9 - 5 + 4 % 2.34 - 1 / 4.25 + 6
 
 # prints text asking a true false question
 puts "Is it true that 3 + 2 < 5 - 7?"
 
 # equation for true false question
-puts 3 + 2 < 5 - 7
+puts 3 + 2.66 < 5 - 7.84
 
 # prints text asking a question and then an equation to answer
-puts "What is 3 + 2? #{3 + 2}"
-puts "What is 5 - 7? #{5 - 7}"
+puts "What is 3 + 2? #{3.7 + 2}"
+puts "What is 5 - 7? #{5 - 7.3}"
 
 # prints text 
 puts "Oh, that's why it's false."
@@ -28,6 +28,6 @@ puts "Oh, that's why it's false."
 puts "How about some more."
 
 # prints text and answers true false question
-puts "Is it greater? #{5 > -2}"
-puts "Is it greater or equal? #{5 >= -2}"
+puts "Is it greater? #{5.22 > -2}"
+puts "Is it greater or equal? #{5 >= -2.39}"
 puts "Is it less or equal? #{5 <= -2}"
