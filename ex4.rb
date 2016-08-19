acc61ed4fe8ef7ac57c3a624d49f4a87ac4a190d
jacob <2staffi@gmail.com>
2016-08-18 22:32:03 -0400

    explained the variable assignments

--- ex4.rb
+++ ex4.rb
@@ -1,10 +1,18 @@
+# number of cars
 cars = 100
+# number of spaces for occupants in the car
 space_in_a_car = 4 # changed 4.0 to .0 to see it affect carpool_capacity 120.0 to 120
+# number of available to drive cars
 drivers = 30
+# people that need transported
 passengers = 90
+# subtracts the number of drivers from the number of cars to show cars not driven
 cars_not_driven = cars - drivers
+# shows that the number of drivers is the same as the number of cars driven
 cars_driven = drivers
+# takes the number of cars driven and the space available to show how many people can go
 carpool_capacity = cars_driven * space_in_a_car
+# takes the number of passengers vs. the number of cars and splits them equally
 average_passengers_per_car = passengers / cars_driven
 
 
