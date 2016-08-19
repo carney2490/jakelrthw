3731c09be5fbccfbc88a3df10f2d7be24eb674b9
jacob <2staffi@gmail.com>
2016-08-19 18:43:23 -0400

    removed redundant letter variables and added notes

--- ex7.rb
+++ ex7.rb
@@ -6,19 +6,17 @@ puts "And everywhere that Mary went."
 puts "." * 10 
 
 # these are the variables for the print and puts lines below
+# took out redundant letters
 end1 = "C"
 end2 = "h"
 end3 = "e"
-end4 = "e"
-end5 = "s"
-end6 = "e"
-end7 = "B"
-end8 = "u"
-end9 = "r"
-end10 = "g"
-end11 = "e"
-end12 = "r"
+end4 = "s"
+end5 = "B"
+end6 = "u"
+end7 = "r"
+end8 = "g"
 
 # print will not add a new line to the end of the output like puts does
-print end1 + end2 + end3 + end4 + end5 + end6
-puts end7 + end8 + end9 + end10 + end11 + end12
+# rewowrked lines 21 nad 20
+print end1 + end2 + end3 + end3 + end4 + end3
+puts end5 + end6 + end7 + end8 + end3 + end7
