5a7e149bcec11f00a4bc64d3211d636a9c7f61db
jacob <2staffi@gmail.com>
2016-08-19 00:34:42 -0400

    added comments

---
+++ ex7.rb
@@ -0,0 +1,24 @@
+# the next 3 puts are just strings
+puts "Mary had a little lamb."
+puts "Its fleece was white as #{'snow'}." # #{'snow'} will do nothing it has no set variable
+puts "And everywhere that Mary went."
+# 10 .
+puts "." * 10 
+
+# these are the variables for the print and puts lines below
+end1 = "C"
+end2 = "h"
+end3 = "e"
+end4 = "e"
+end5 = "s"
+end6 = "e"
+end7 = "B"
+end8 = "u"
+end9 = "r"
+end10 = "g"
+end11 = "e"
+end12 = "r"
+
+# print will not add a new line to the end of the output like puts does
+print end1 + end2 + end3 + end4 + end5 + end6
+puts end7 + end8 + end9 + end10 + end11 + end12
