99cbad10f327040efd3ec5bb81951e8864485699
jacob <2staffi@gmail.com>
2016-08-18 22:49:16 -0400

    init ex5.rb

---
+++ ex5.rb
@@ -0,0 +1,16 @@
+my_name = 'Jacob C. Carroll'
+my_age = 33 # in 2016
+my_height = 71 # inches
+my_weight = 360 # lbs
+my_eyes = 'Brown'
+my_teeth = 'White'
+my_hair = 'Brown'
+
+puts "Let's talk about #{my_name}."
+puts "He's #{my_height} inches tall."
+puts "He's #{my_height} pounds heavy."
+puts "Actually that's not heavy, it's just fat."
+puts "He's got #{my_eyes} eyes and #{my_hair} hair."
+puts "His teeth are usually #{my_teeth} depending on the coffee."
+
+puts "If I add #{my_age}, #{my_height}, #{my_weight} I get #{my_age + my_height + my_weight}."
