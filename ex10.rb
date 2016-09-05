9a92d8b37eafc854ea07a61159262d0727cc603b
jacob <2staffi@gmail.com>
2016-09-05 01:11:47 -0400

    adden comments

--- ex10.rb
+++ ex10.rb
@@ -1,7 +1,12 @@
+# set tabby_cat variable to a string that is tabbed in when printed
 tabby_cat = "\tI'm tabbed in."
+# set persian_cat variable to a string split on two different lines
 persian_cat = "I'm split\non a line."
+# set backslash_cat variable to string that adds two \ around a
 backslash_cat = "I'm \\ a \\ cat."
 
+# sets fat_cat variable to a multi line string that is tabbed and has one 
+# new line that is also tabbed
 fat_cat = """
 I'll do a list:
 \t* Cat food
@@ -9,6 +14,7 @@ I'll do a list:
 \t* Catnip\n\t* Grass
 """
 
+# puts all the different variables
 puts tabby_cat
 puts persian_cat
 puts backslash_cat
