6640b48fbf255138e0a5f3a285e0864d9d9790a5
jacob <2staffi@gmail.com>
2016-09-05 00:58:10 -0400

    added comments

--- ex9.rb
+++ ex9.rb
@@ -1,11 +1,17 @@
 # Here's some new strange stuff, remember type it exactly.
 
+# sets days eual to a string listing the days with a space separating each
 days = "Mon Tue Wed Thu Fri Sat Sun"
+# sets months equal to the months using \n to put each on a new line
 months = "Jan\nFeb\nMar\nApr\nMay\nJun\nJul\nAug"
 
+# prints a string with #{} calling on variable days
 puts "Here are the days: #{days}"
+# prints a string with #{} calling on variable months
 puts "Here are the months: #{months}"
 
+# uses a 3 """ to type a sting on multiple lines ending in another 3 """
+# which shows how it is typed
 puts """
 There's something going on here.
 With the three double-quotes.
